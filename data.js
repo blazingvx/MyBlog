891bbf94b9276e327bb41f7d0f64e3e5b0f67aca
blazingvx <backupworkvx@gmail.com>
2023-03-26 21:16:33 +0530

    Dark-Theme

--- data.js
+++ data.js
@@ -6,6 +6,114 @@ const blogPosts = [
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
     },
     {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My First Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
       title: "My Second Blog Post",
       date: "March 15, 2023",
       author: "Jane Smith",
