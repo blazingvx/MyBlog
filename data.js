49d8dcdde558ce81a6e3532b45d7a637bd4a038a
blazingvx <backupworkvx@gmail.com>
2023-03-26 11:50:49 +0530

    Test Blog

---
+++ data.js
@@ -0,0 +1,15 @@
+const blogPosts = [
+    {
+      title: "My Third Blog Post",
+      date: "March 1, 2023",
+      author: "John Doe",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    },
+    {
+      title: "My Fourth Blog Post",
+      date: "March 15, 2023",
+      author: "Jane Smith",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
+    }
+  ];
+  
