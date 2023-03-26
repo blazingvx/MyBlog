7711f375eba5e21ad256fbf74a95959342aaf503
blazingvx <backupworkvx@gmail.com>
2023-03-26 20:11:09 +0530

    Remove Extra Blogs

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 const blogPosts = [
     {
-      title: "My Third Blog Post",
+      title: "My First Blog Post",
       date: "March 1, 2023",
       author: "John Doe",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
     },
     {
-      title: "My Fourth Blog Post",
+      title: "My Second Blog Post",
       date: "March 15, 2023",
       author: "Jane Smith",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien a lacinia hendrerit, sapien ligula bibendum risus, in interdum augue sapien ut purus. Sed efficitur urna non turpis bibendum bibendum."
